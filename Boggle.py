ec200606e0601cdf12a518bc3d2793b7b24b7d8e
ssarver <ssarver@rollins.edu>
2017-02-27 03:30:32 +0000

    Added Boggle Problem

---
+++ Boggle.py
@@ -0,0 +1,11 @@
+# Project 2 - Solving Problems by Searching: Boggle
+# Sierra Sarver 2/26/17
+
+# Actual boggle game board to test
+board = [['u', 'n', 't', 'h'],
+         ['g', 'a', 'e', 's'],
+         ['s', 'r', 't', 'r'],
+         ['h', 'm', 'i', 'a']]
+
+# Create a dictionary that stores all prefixes in the word list
+
